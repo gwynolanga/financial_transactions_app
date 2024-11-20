495434020f3fe6cee792e8c499968b9f51e8f1c9
Kirill Ilin <gwynolanga@gmail.com>
2024-11-20 03:28:28 +0000

    Update model and migration for Account class

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,7 +1,8 @@
 class Account < ApplicationRecord
   belongs_to :currency
-  belongs_to :holder, class_name: 'User', foreign_key: 'holder_id'
+  belongs_to :user
 
-  validates :number, presence: true, uniqueness: true
+  validates :number, presence: true, uniqueness: true, length: { is: 16 }
   validates :balance, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :user_id, uniqueness: { scope: :currency_id }
 end
--- db/migrate/20241120030129_create_accounts.rb
+++ db/migrate/20241120030129_create_accounts.rb
@@ -1,14 +1,19 @@
+# frozen_string_literal: true
+
 class CreateAccounts < ActiveRecord::Migration[7.1]
   def change
     create_table :accounts do |t|
       t.string :number, null: false
       t.decimal :balance, precision: 10, scale: 2, null: false, default: 0
       t.references :currency, null: false, foreign_key: true
-      t.references :holder, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
-    end
 
-    add_index :accounts, :number, unique: true
+      t.index :number, unique: true
+      t.index %i[user_id currency_id], unique: true
+      t.check_constraint 'char_length(number) = 16', name: 'number_check'
+      t.check_constraint 'balance >= 0', name: 'balance_check'
+    end
   end
 end
