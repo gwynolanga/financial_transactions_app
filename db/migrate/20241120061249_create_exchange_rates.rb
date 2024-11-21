5b068cb8597d4c1e87573346b13da0b8989d3a17
Kirill Ilin <gwynolanga@gmail.com>
2024-11-21 10:24:44 +0000

    Fix column name for ExchangeRate migration

--- db/migrate/20241120061249_create_exchange_rates.rb
+++ db/migrate/20241120061249_create_exchange_rates.rb
@@ -3,7 +3,7 @@
 class CreateExchangeRates < ActiveRecord::Migration[7.1]
   def change
     create_table :exchange_rates do |t|
-      t.decimal :amount, precision: 2, scale: 2, null: false
+      t.decimal :value, precision: 2, scale: 16, null: false
       t.references :base_currency, null: false, foreign_key: { to_table: :currencies }
       t.references :target_currency, null: false, foreign_key: { to_table: :currencies }
 
@@ -11,7 +11,7 @@ class CreateExchangeRates < ActiveRecord::Migration[7.1]
 
       t.index 'LEAST(base_currency_id, target_currency_id), GREATEST(base_currency_id, target_currency_id)',
               unique: true, name: 'index_exchange_rates_on_normalized_pair'
-      t.check_constraint 'amount > 0', name: 'chk_exchange_rates_amount_positive'
+      t.check_constraint 'value > 0', name: 'chk_exchange_rates_value_positive'
     end
   end
 end
