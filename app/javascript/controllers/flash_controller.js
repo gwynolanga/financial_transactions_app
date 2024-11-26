3ea0e8d98e687398fc3a92df37fc8f9786722214
Kirill Ilin <gwynolanga@gmail.com>
2024-11-26 18:52:31 +0000

    Add flash messages

---
+++ app/javascript/controllers/flash_controller.js
@@ -0,0 +1,12 @@
+// app/javascript/controllers/flash_controller.js
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+    dismiss() {
+        this.element.remove();
+    }
+
+    connect() {
+        setTimeout(() => this.dismiss(), 5000);
+    }
+}
