c8cf76b6562aa03143c32b5cbc82dc17f9a6486f
Kirill Ilin <gwynolanga@gmail.com>
2024-12-19 22:59:51 +0000

    Fix some bugs for navbar and flatpickr

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -4,6 +4,7 @@ import flatpickr from "flatpickr"
 export default class extends Controller {
     connect() {
         this.picker = flatpickr(this.element, {
+            disableMobile: "true", // Fix a weird behaviour (broken datepicker) for mobile screen size
             enableTime: true,
             dateFormat: "Y-m-d H:i",
             minDate: "today"
--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -3,15 +3,29 @@ import { Controller } from "@hotwired/stimulus";
 export default class extends Controller {
     static targets = ["links", "innerMenu"];
 
+    // Fix a weird behaviour after redirecting to another page
+    disconnect() {
+        if (this.hasInnerMenuTarget) {
+            this.innerMenuTarget.remove();
+        }
+    }
+
     toggle() {
         this.linksTarget.classList.toggle("hidden");
     }
 
-    dropdown(event) {
+    openDropdown(event) {
         event.preventDefault();
 
         if (this.hasInnerMenuTarget) {
             this.innerMenuTarget.classList.toggle("hidden");
+        }
+    }
+
+    // Close the innerMenu target by LMB clicking outside of that target
+    closeDropdown(event) {
+        if (!this.element.contains(event.relatedTarget)) {
+            this.innerMenuTarget.classList.add("hidden");
         }
     }
 }
