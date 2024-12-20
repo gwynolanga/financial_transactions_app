0178e11ee5d71f72103bedcee109d25edf964270
Kirill Ilin <gwynolanga@gmail.com>
2024-12-20 13:50:12 +0000

    Rework again dynamic form for all kinds of transaction

--- app/helpers/transactions_helper.rb
+++ app/helpers/transactions_helper.rb
@@ -1,4 +1,63 @@
 # frozen_string_literal: true
 
 module TransactionsHelper
+  def transaction_field_set(transaction, form, account)
+    fields = []
+    if transaction.immediate? || transaction.scheduled?
+      fields.concat(immediate_and_scheduled_fields(transaction, form, account))
+    end
+    fields.concat(scheduled_fields(transaction, form)) if transaction.scheduled?
+    fields.concat(deposit_fields(transaction, form)) if transaction.deposit?
+    fields.concat(withdrawal_fields(transaction, form)) if transaction.withdrawal?
+    fields
+  end
+
+  private
+
+  def immediate_and_scheduled_fields(transaction, form, account)
+    [
+      field(:amount, form, :sender_amount, transaction.errors[:sender_amount]),
+      field(:select, form, :recipient_id, recipient_errors(transaction), recipient_options(account))
+    ]
+  end
+
+  def scheduled_fields(transaction, form)
+    [
+      field(:datetime, form, :execution_date, transaction.errors[:execution_date])
+    ]
+  end
+
+  def deposit_fields(transaction, form)
+    [
+      field(:amount, form, :recipient_amount, transaction.errors[:recipient_amount])
+    ]
+  end
+
+  def withdrawal_fields(transaction, form)
+    [
+      field(:amount, form, :sender_amount, transaction.errors[:sender_amount])
+    ]
+  end
+
+  def field(type, form, attribute, errors, options = nil)
+    {
+      type: type,
+      locals: {
+        form: form,
+        attribute: attribute,
+        errors: errors,
+        options: options
+      }
+    }
+  end
+
+  def recipient_options(account)
+    Account.where.not(id: account.id).map do |a|
+      ["#{a.human_number} / #{a.currency_name} / #{a.user_full_name}", a.id]
+    end
+  end
+
+  def recipient_errors(transaction)
+    transaction.errors[:recipient_id] + transaction.errors[:recipient]
+  end
 end
--- app/javascript/controllers/transaction_form_controller.js
+++ app/javascript/controllers/transaction_form_controller.js
@@ -1,66 +1,36 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-    static targets = ["immediate", "scheduled", "deposit", "withdrawal"];
+    static targets = ["transactionKindRadios", "transactionFields"];
 
     connect() {
-        this.resetRadioButtons();
+        // After refreshing the page in the browser by pressing the F5 key on the keyboard
+        window.addEventListener('load', this.resetForm.bind(this));
     }
 
-    toggle(event) {
-        const selectedType = event.target.value;
-        const targets = {
-            immediate: this.immediateTarget,
-            scheduled: this.scheduledTarget,
-            deposit: this.depositTarget,
-            withdrawal: this.withdrawalTarget,
-        };
+    visitUrl(event) {
+        const url = event.target.dataset.url;
+        const turboFrame = event.target.dataset.turboFrame;
 
-        Object.entries(targets).forEach(([type, target]) => {
-            const isHidden = selectedType !== type;
-
-            target.classList.toggle("hidden", isHidden);
-
-            if (isHidden) {
-                this.clearInputs(target);
-            } else {
-                this.setFocus(target);
+        if (url && turboFrame) {
+            const frame = document.getElementById(turboFrame);
+            if (frame) {
+                frame.src = url;
+                window.history.pushState({}, "", url);
             }
-        });
-    }
-
-    clearInputs(target) {
-        target.querySelectorAll("label").forEach((label) => label.classList.remove("error"));
-
-        target.querySelectorAll("input, textarea, select").forEach((input) => {
-            if (input.type === "checkbox" || input.type === "radio") {
-                input.checked = false;
-            } else {
-                input.value = "";
-            }
-
-            input.classList.remove("error");
-
-            const errorMessage = input.nextElementSibling;
-            if (errorMessage && errorMessage.classList.contains("form-error-messages")) {
-                errorMessage.textContent = "";
-            }
-        });
-    }
-
-    resetRadioButtons() {
-        const radioButtons = this.element.querySelectorAll('input[type="radio"][name="transaction[kind]"]');
-        if (radioButtons.length === 0) return;
-
-        const firstRadio = Array.from(radioButtons).find((radio) => radio.checked) || radioButtons[0];
-        if (firstRadio) {
-            firstRadio.checked = true;
-            firstRadio.dispatchEvent(new Event("change"));
         }
     }
 
-    setFocus(target) {
-        const firstInput = target.querySelector("input:not([disabled]), textarea:not([disabled]), select:not([disabled])");
-        firstInput?.focus();
+    resetForm() {
+        const radios = this.transactionKindRadiosTarget.querySelectorAll('input');
+        const fields = this.transactionFieldsTarget.querySelectorAll('input, select, textarea');
+
+        radios.forEach(radio => {
+            radio.checked = radio.dataset.default === "true"
+        });
+
+        fields.forEach(field => {
+            field.value = field.dataset.default;
+        });
     }
 }
